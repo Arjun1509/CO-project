2298dcab8c204256b3cacf5fef585c165cd4e6fd
devesh1psy <devesh.bgs@gmail.com>
2026-03-04 23:46:38 +0530

    added the input reading function

--- co_2026_evaluation_framework_release/SimpleAssembler/assembler.py
+++ co_2026_evaluation_framework_release/SimpleAssembler/assembler.py
@@ -3,4 +3,19 @@ import sys
 def error(ln_num, msg):
 #prints error as per file
     print(f"error at line{ln_num} : {msg}" )
-    sys.exit(0) 
+    sys.exit(0)
+
+def input_reader():
+#reads the input 
+    if len(sys.argv) != 3:
+        print("invalid arguments")
+        sys.exit(0)
+
+    ifile = sys.argv[1]
+    ofile = sys.argv[2]
+
+    with open(ifile, "r") as f:
+        lines = [line.strip() for line in f if line.strip() != ""]
+
+    return lines, ofile
+
